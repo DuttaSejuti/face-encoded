ca5664896ae75ad4319f2c2205f8337075c81d65
DuttaSejuti <duttasejuti24@gmail.com>
2026-04-14 18:12:06 +0200

    use alembic for migrations

--- src/main.py
+++ src/main.py
@@ -1,22 +1,10 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.app.core.config import PROJECT_NAME, API_V1_STR
 from src.app.api.v1 import sessions
-from src.db.session import engine
-from src.db.base import Base
-from src.db import models
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-    await engine.dispose()
 
 app = FastAPI(
     title=PROJECT_NAME,
     openapi_url=f"{API_V1_STR}/openapi.json",
-    lifespan=lifespan
 )
 
 app.include_router(sessions.router, prefix=f"{API_V1_STR}/sessions", tags=["sessions"])
